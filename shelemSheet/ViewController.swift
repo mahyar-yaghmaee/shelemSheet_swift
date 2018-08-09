b944052c99795d4f30e2b162c625beeb87d84609
Mahyar Yaghmaee <mahyar.yaghmaee@gmail.com>
2018-08-08 19:26:11 -0700

    validate call number in a function

--- shelemSheet/ViewController.swift
+++ shelemSheet/ViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
-
+    var isCallTime: Bool = true // this is to show which state we are in, starting with CALL state (vs Submit Points state)
     @IBOutlet var teamAPointsLabel:UILabel!
     @IBOutlet var teamBPointsLabel:UILabel!
     @IBOutlet var teamApointsInputtText:UITextField!
@@ -19,26 +20,39 @@ class ViewController: UIViewController {
     
     @IBAction func showPoints(sender: AnyObject){
 
-
         let amountTeamA:String! = teamApointsInputtText.text
-        print("Hello!: ", amountTeamA)
-        if (Int(amountTeamA)! > 165 || Int(amountTeamA)! < 100 || Int(amountTeamA)!%5 != 0){
-            //|| Int(amountTeamA)! > 100 || Int(amountTeamA)!%5 != 0
-            createAlert(title: "Invalid Number", message: "CALL range : 100 to 165 (5 point slices)")
-        }
-        else{
-            pointsSlider.value = Float(amountTeamA)!/165
-            pointsSlider.isEnabled = false
-            teamAPointsLabel.text = teamApointsInputtText.text
-            teamBPointsLabel.text = teamBpointsInputtText.text
-        }
+        let amountTeamB:String! = teamBpointsInputtText.text
+        if isCallTime{
+            // only one value should be entered (teamA or teamB)
+            if (amountTeamA.isEmpty == amountTeamB.isEmpty){
+                createAlert(title: "Invalid CALL input", message: "Only one team should CALL")
+            }
+            else{
+                if (isCallNumberValid(number: Int(amountTeamA)!)){
+                    pointsSlider.value = Float(amountTeamA)!/165
+                    pointsSlider.isEnabled = false
+                    teamAPointsLabel.text = teamApointsInputtText.text
+                    teamBPointsLabel.text = teamBpointsInputtText.text
+                    isCallTime = !isCallTime
+                }
+                else{
+                    createAlert(title: "Invalid Number", message: "CALL range : 100 to 165 (5 point slices)")
 
+                }
+            }
+        }
     
+    }
+    
+    func isCallNumberValid(number: Int) -> Bool {
+        if (number < 170 && number > 100 && number%5 == 0){
+            return true
+        }
+        return false
     }
     
 
     func createAlert(title:String, message:String) -> Void {
-     //   let alertUI = UIAlertController(nibName: title, bundle: message)
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: {(action) in
